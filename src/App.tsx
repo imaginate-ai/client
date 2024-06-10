b437d7cb63ec55cf50d3cc79cb25b27338b5a4e3
Zach Legesse <47370318+zachale@users.noreply.github.com>
2024-06-09 22:26:44 -0400

    1 build proof of concept page (#2)
    * added tailwind css support
    
    * implemented basic photo queue with action buttons #1
    
    * readme update #1
    
    * added navbar #1
    
    * readme update #1
    
    * Implemented @faisal-fawad change request

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { FC, useState } from 'react'
 import './App.css'
+import { PhotoQueueProps } from './interfaces/PhotoQueueProps'
+import { testImages } from './testData'
+import Navbar from './navigation/Navbar';
 
-function App() {
-  const [count, setCount] = useState(0)
+const PhotoQueue = ({ images }: PhotoQueueProps): JSX.Element => {
+  const [index, setIndex] = useState(0);
+
+  const makeChoice = (choseGenerated: boolean) => {
+
+    const correctChoice = (choseGenerated == images[index].generated)
+
+    if (correctChoice) {
+      console.log("Correct!")
+      //increase score
+    }
+
+    if (index < images.length - 1) {
+      setIndex(index + 1)
+    } else {
+      //handle game over
+    }
+
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-10/12 h-full mt-10'>
+      <div className='flex justify-center max-h-1/2'>
+        <img className='w-auto mb-2 rounded-lg h-full' src={images[index].url} />
       </div>
-      <h1>Imaginate</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className='block h-1/3'>
+        <button className='w-full mb-2' onClick={() => { makeChoice(false) }}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button className='w-full mb-2' onClick={() => { makeChoice(true) }}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+          </svg>
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  )
+}
+
+
+function App() {
+
+  return (
+    <div className='w-screen h-screen'>
+      <Navbar />
+      <div className='page-content flex justify-center'>
+        <PhotoQueue images={testImages} />
+      </div>
+
+
+    </div>
   )
 }
 
---
+++ src/testData.ts
@@ -0,0 +1,24 @@
+import { Image } from "./interfaces/imageInterface";
+
+export const testImages: Image[] = [
+  {
+    url: "https://media.discordapp.net/attachments/1231667681862619309/1248764484713971813/output_test.jpg?ex=666582ce&is=6664314e&hm=2dbf115ba3eb9f150f069f5c0b67cd7e5614da37c542c126af2046564ba696c1&=&format=webp",
+    generated: false
+  },
+  {
+    url: "https://media.discordapp.net/attachments/1231667681862619309/1248770877642899578/image.png?ex=666588c2&is=66643742&hm=4303d4293e138f47574cf7cc0fb4aa70458b36da6f730c2375313c5244fd3a30&=&format=webp&quality=lossless",
+    generated: false
+  },
+  {
+    url: "https://media.discordapp.net/attachments/1231667681862619309/1248784320500334602/image.png?ex=66659547&is=666443c7&hm=8b0f572bc149ec5bfb9a99dc9f6f15c1f84c75eed5ba940799a02934e05e7ac5&=&format=webp&quality=lossless",
+    generated: false
+  },
+  {
+    url: "https://media.discordapp.net/attachments/1231667681862619309/1247638850608562297/generated_image.png?ex=666364b9&is=66621339&hm=26beda6e34c8002c4987e7b68cf6052e6df3bde19dd6cde4a27194751fb41a88&=&format=webp&quality=lossless",
+    generated: false
+  },
+  {
+    url: "https://media.discordapp.net/attachments/1231667681862619309/1247638851078455336/input.png?ex=666364ba&is=6662133a&hm=8a403a9b950610f3c61163f2f4f853a1e95f6aa08130d2cd8e70c0ac535a249c&=&format=webp&quality=lossless",
+    generated: false
+  }
+]
