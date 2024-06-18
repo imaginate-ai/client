348be46a408dd57344cb44c2854c95506170d491
numba19 <zachlegesse@gmail.com>
2024-06-18 19:53:05 -0400

    added web icon and made test data local #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import { PhotoQueueProps } from './interfaces/PhotoQueueProps'
 import { testImages } from './testData'
--- src/navigation/Navbar.tsx
+++ src/navigation/Navbar.tsx
@@ -4,13 +4,13 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div className='flex '>
-      <img className="h-10 m-4 cursor-pointer" src={logo} />
-      <div className='item inline-block rounded-full h-1/2 align-baseline mt-auto mb-auto ml-2'>
+      <img className="h-10 m-4" src={logo} />
+      {/* <div className='item inline-block rounded-full h-1/2 align-baseline mt-auto mb-auto ml-2'>
         <p className='nav-font'>Daily</p>
       </div>
       <div className='item inline-block rounded-full h-1/2 align-baseline mt-auto mb-auto ml-2'>
         <p className='nav-font'>+</p>
-      </div>
+      </div> */}
     </div >
   );
 };
