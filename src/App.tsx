d619822d2ab8107bc8cdd84322a827591b5e9265
Zach Legesse <47370318+zachale@users.noreply.github.com>
2024-09-02 16:26:34 -0400

    6 add x and check mark feedback (#7)
    * added web icon and made test data local #3
    
    * added ant design UI package #3
    
    * added modal on game over with score #3
    
    * minor style updates #3
    
    * front end work #3
    
    * Added improved button feedback #3
    
    * added button feedback, improved styling and code formating #3
    
    * initial commit #6
    
    * Added X and Check feedback overlay, added crude photo counter #6
    
    * added modal on game over with score #3
    
    * minor style updates #3
    
    * front end work #3
    
    * Added improved button feedback #3
    
    * initial commit #6
    
    * added photo counter and feedback overlay #6

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, JSX, useRef } from 'react';
+import { useState, JSX, useRef, ReactElement } from 'react';
 import './App.css';
 import {
   PhotoQueueProps,
@@ -7,7 +7,7 @@ import {
 import { testImages } from './testData';
 import Navbar from './navigation/Navbar';
 import { ConfigProvider, theme, Modal, Button } from 'antd';
-
+import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
 const PhotoQueueButtons = ({
   makeChoice,
   disabled,
@@ -66,7 +66,7 @@ const PhotoQueueButtons = ({
 
   return (
     <>
-      <div className='flex flex-row gap-2 h-16'>
+      <div className='flex flex-row gap-8 h-16'>
         <Button
           ref={AiButton}
           type='primary'
@@ -95,6 +95,8 @@ const PhotoQueue = ({ images }: PhotoQueueProps): JSX.Element => {
   const [scoreText, setScoreText] = useState('');
   const [disableButtons, setDisableButtons] = useState(false);
   const shareButton = useRef<HTMLElement>(null);
+  const image = useRef<HTMLElement>(null);
+  const [feedbackOverlay, setFeedbackOverlay] = useState<ReactElement>();
 
   const makeChoice = (choseGenerated: boolean, choiceCallBack: Function) => {
     const isCorrectChoice = choseGenerated == images[index].generated;
@@ -102,8 +104,18 @@ const PhotoQueue = ({ images }: PhotoQueueProps): JSX.Element => {
     if (isCorrectChoice) {
       setScore(score + 1);
       setScoreText(scoreText + '🟩');
+      setFeedbackOverlay(
+        <div className='absolute w-full h-full content-center text-center bg-green-500'>
+          <CheckOutlined className='text-9xl text-green-800' />
+        </div>,
+      );
     } else {
       setScoreText(scoreText + '🟥');
+      setFeedbackOverlay(
+        <div className=' absolute w-full h-full content-center text-center bg-red-500'>
+          <CloseOutlined className='text-9xl text-red-800' />
+        </div>,
+      );
     }
 
     setTimeout(() => {
@@ -113,6 +125,7 @@ const PhotoQueue = ({ images }: PhotoQueueProps): JSX.Element => {
         setIsModalOpen(true);
         setDisableButtons(true);
       }
+      setFeedbackOverlay(undefined);
       choiceCallBack();
     }, 750);
 
@@ -147,16 +160,22 @@ const PhotoQueue = ({ images }: PhotoQueueProps): JSX.Element => {
 
   return (
     <div className='w-10/12 h-full'>
-      <div className='flex justify-center'>
-        <img
-          className='w-auto mb-2 rounded-lg h-full'
-          src={images[index].url}
-        ></img>
+      <div
+        className='flex justify-center relative rounded-lg overflow-hidden mb-8'
+        style={{ backgroundColor: undefined }}
+      >
+        <img ref={image} className='w-auto h-full' src={images[index].url} />
+        <div className='opacity-75 absolute w-full h-full'>
+          {feedbackOverlay}
+        </div>
       </div>
       <PhotoQueueButtons
         makeChoice={makeChoice}
         disabled={disableButtons}
       ></PhotoQueueButtons>
+      <h1 className='text-body text-center mt-4'>
+        {index + ' / ' + images.length}
+      </h1>
       <Modal
         title='Well Played!'
         open={isModalOpen}
