084dd17062a52a1db454aae29113e2ebffabd9dc
numba19 <zachlegesse@gmail.com>
2024-09-02 16:28:41 -0400

    Type and import fix #9

--- src/testData.ts
+++ src/testData.ts
@@ -1,30 +1,29 @@
-import { Image } from "./interfaces/ImageInterface";
-import cat1 from './assets/cat1.png'
-import cat2 from './assets/cat2.png'
-import cat3 from './assets/cat3.jpg'
-import cat4 from './assets/cat4.jpg'
-import cat5 from './assets/cat5.png'
-
+import { Image } from './interfaces/ImageInterface.ts';
+import cat1 from './assets/cat1.png';
+import cat2 from './assets/cat2.png';
+import cat3 from './assets/cat3.jpg';
+import cat4 from './assets/cat4.jpg';
+import cat5 from './assets/cat5.png';
 
 export const testImages: Image[] = [
   {
     url: cat1,
-    generated: true
+    generated: true,
   },
   {
     url: cat2,
-    generated: true
+    generated: true,
   },
   {
     url: cat3,
-    generated: false
+    generated: false,
   },
   {
     url: cat4,
-    generated: true
+    generated: true,
   },
   {
     url: cat5,
-    generated: true
-  }
-]
+    generated: true,
+  },
+];
