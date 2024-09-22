1f397fe9165110e80c3396ad6165e046cf686482
Zach Legesse <47370318+zachale@users.noreply.github.com>
2024-09-22 18:40:27 -0400

    Shuffle images randomly based on day seed #18 (#19)

--- src/services/imageBackend.ts
+++ src/services/imageBackend.ts
@@ -1,18 +1,22 @@
-const API_URL = 'https://o7hgv46qcf7swox3ygn53tayay0zzhgl.lambda-url.us-east-1.on.aws/';
+const API_URL =
+  'https://o7hgv46qcf7swox3ygn53tayay0zzhgl.lambda-url.us-east-1.on.aws/';
 const START_DATE = 1725148800000; //september 1st 2024 in timestamp form in miliseconds
 const MS_PER_DAY = 86400000;
 
 export const getImages = async () => {
   const day = _getDay();
   const images: any[] = await fetch(
-    API_URL + '?' +
+    API_URL +
+      '?' +
       new URLSearchParams({
         day: `${day}`,
       }).toString(),
-  ).then((res) => res.json()).catch((error)=>{
-    console.error(error)
-  });
-  return images;
+  )
+    .then((res) => res.json())
+    .catch((error) => {
+      console.error(error);
+    });
+  return _shuffle(images, day);
 };
 
 const _getDay = () => {
@@ -21,3 +25,22 @@ const _getDay = () => {
   const msSinceStartDate = today - startDate;
   return msSinceStartDate / MS_PER_DAY;
 };
+
+function _shuffle(array: any[], seed: number) {
+  var m = array.length,
+    t,
+    i;
+  while (m) {
+    i = Math.floor(_random(seed) * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+    ++seed;
+  }
+  return array;
+}
+
+function _random(seed: number) {
+  var x = Math.sin(seed++) * 10000;
+  return x - Math.floor(x);
+}
