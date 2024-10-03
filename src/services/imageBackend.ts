5f99e66c64f131639f94d12b12d81c698225ccfa
Nathan Probert <109328434+nathan-probert@users.noreply.github.com>
2024-10-03 00:52:31 -0400

    Fix timezone issue (#30)
    * change start date per timezone
    
    * change number as well

--- src/services/imageBackend.ts
+++ src/services/imageBackend.ts
@@ -1,6 +1,5 @@
-const API_URL =
-  'https://o7hgv46qcf7swox3ygn53tayay0zzhgl.lambda-url.us-east-1.on.aws/';
-const START_DATE = 1725148800000; //september 1st 2024 in timestamp form in miliseconds
+const API_URL = 'https://o7hgv46qcf7swox3ygn53tayay0zzhgl.lambda-url.us-east-1.on.aws/';
+const START_DATE = new Date(2024, 8, 1).setHours(0, 0, 0, 0);  //september 1st 2024 in timestamp form in miliseconds of local timezone
 const MS_PER_DAY = 86400000;
 
 export const getImages = async () => {
@@ -20,10 +19,9 @@ export const getImages = async () => {
 };
 
 const _getDay = () => {
-  const startDate = new Date(START_DATE).setHours(0, 0, 0, 0);
   const today = new Date().setHours(0, 0, 0, 0);
-  const msSinceStartDate = today - startDate;
-  return msSinceStartDate / MS_PER_DAY;
+  const msSinceStartDate = today - START_DATE;
+  return Math.floor(msSinceStartDate / MS_PER_DAY);
 };
 
 function _shuffle(array: any[], seed: number) {
