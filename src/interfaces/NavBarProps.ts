1d4ab0398ef6f09f5f509b6d7d3bad854444cfed
Zach Legesse <zachlegesse@gmail.com>
2024-10-27 23:34:48 -0400

    Removed unnecessary interfaces #33

--- src/interfaces/NavBarProps.ts
+++
@@ -1,3 +0,0 @@
-export interface NavBarProps {
-  theme: string | undefined;
-}
--- src/photoQueue/interfaces/ImageInterface.ts
+++
@@ -1,9 +0,0 @@
-export interface Image {
-  data: string;
-  date: string;
-  filename: string;
-  real: boolean;
-  status: 'verified';
-  theme: string;
-  url: string;
-}
