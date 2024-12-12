e30468d50bb9cf4b7b3d416aa0f357ca7346fcf4
numba19 <zachlegesse@gmail.com>
2024-12-11 19:27:29 -0500

    Minor fixes #35

--- src/components/Auth/AuthButton.tsx
+++ src/components/Auth/AuthButton.tsx
@@ -1,10 +1,9 @@
-import { useAuth } from '../../providers/AuthProvider';
-import SignOutButton from './SignOutButton';
-import LoginButton from './LoginButton';
+import { useAuth } from "../../providers/AuthProvider";
+import SignOutButton from "./SignOutButton";
+import LoginButton from "./LoginButton";
 
 const AuthButton = () => {
   const auth = useAuth();
-
   return auth?.user ? <SignOutButton /> : <LoginButton />;
 };
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,33 +1,33 @@
-import { Divider, Flex, Tooltip } from 'antd';
-import { InfoCircleOutlined } from '@ant-design/icons';
-import logo from '../../assets/imaginate-logo.png';
-import './NavBar.css';
-import { useAuth } from '../../providers/AuthProvider';
-import AuthButton from '../Auth/AuthButton';
+import { Divider, Flex, Tooltip } from "antd";
+import { InfoCircleOutlined } from "@ant-design/icons";
+import logo from "../../assets/imaginate-logo.png";
+import "./NavBar.css";
+import AuthButton from "../Auth/AuthButton";
 
-const THEME_EXPLAINER_TEXT = 'The theme changes every day!';
+const THEME_EXPLAINER_TEXT = "The theme changes every day!";
 
 type NavBarProps = {
   theme: string | undefined;
 };
 
 const NavBar = ({ theme }: NavBarProps) => {
-  const auth = useAuth();
   return (
-    <div className='w-full px-4'>
-      <Flex align='flex-end' justify='space-between' wrap>
-        <img className='h-10 mt-6' src={logo} />
-        <Flex align='center' justify='space-between' gap={'16px'}>
-          <Tooltip title={THEME_EXPLAINER_TEXT} placement='bottomLeft'>
-            {theme ? (
-              <Flex>
-                <InfoCircleOutlined className='mr-1' />
-                <p className='mr-2'>Theme:</p>
-                <p>{theme}</p>
-              </Flex>
-            ) : (
-              ''
-            )}
+    <div className="w-full px-4">
+      <Flex align="flex-end" justify="space-between" wrap>
+        <img className="h-10 mt-6" src={logo} />
+        <Flex align="center" justify="space-between" gap={"16px"}>
+          <Tooltip title={THEME_EXPLAINER_TEXT} placement="bottomLeft">
+            {theme
+              ? (
+                <Flex>
+                  <InfoCircleOutlined className="mr-1" />
+                  <p className="mr-2">Theme:</p>
+                  <p>{theme}</p>
+                </Flex>
+              )
+              : (
+                ""
+              )}
           </Tooltip>
           <AuthButton />
         </Flex>
