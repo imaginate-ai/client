04b1b7c55ef75a84bd3da1892c00ae4a25ffd7ba
numba19 <zachlegesse@gmail.com>
2024-12-13 18:45:27 -0500

    HOTFIX: twitter impersonation bandaid

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState, useRef, ReactElement, useEffect } from 'react';
-import './App.css';
-import Navbar from './navigation/Navbar';
-import { ConfigProvider, theme, Flex } from 'antd';
+import { ReactElement, useEffect, useRef, useState } from "react";
+import "./App.css";
+import Navbar from "./navigation/Navbar";
+import { Alert, ConfigProvider, Divider, Flex, theme } from "antd";
 
-import posthog from 'posthog-js';
-import Cookies from 'universal-cookie';
-import { PhotoQueue } from './photoQueue/photoQueue.tsx';
-import { getImages } from './services/imageBackend.ts';
-import { Image } from './photoQueue/interfaces/ImageInterface.ts';
+import posthog from "posthog-js";
+import Cookies from "universal-cookie";
+import { PhotoQueue } from "./photoQueue/photoQueue.tsx";
+import { getImages } from "./services/imageBackend.ts";
+import { Image } from "./photoQueue/interfaces/ImageInterface.ts";
 
 const cookies = new Cookies();
 
@@ -16,7 +16,7 @@ function App() {
   const [showApp, setShowApp] = useState(true);
   const savedScoreString = useRef<ReactElement>();
   const [images, setImages] = useState<Image[]>([]);
-  const [imageTheme, setImageTheme] = useState('');
+  const [imageTheme, setImageTheme] = useState("");
 
   useEffect(() => {
     getImages().then((response: Image[]) => {
@@ -28,12 +28,12 @@ function App() {
   }, []);
 
   const splitNewlinesToParagraphTags = (input: string) => {
-    return input.split('\n').map((text) => <p key={text}>{text}</p>);
+    return input.split("\n").map((text) => <p key={text}>{text}</p>);
   };
 
   useEffect(() => {
-    const dayLastPlayed = new Date(cookies.get('day_last_played'));
-    const completeScoreText: string = cookies.get('last_complete_score_text');
+    const dayLastPlayed = new Date(cookies.get("day_last_played"));
+    const completeScoreText: string = cookies.get("last_complete_score_text");
     const today = new Date();
     today.setHours(0, 0, 0, 0);
     if (
@@ -48,71 +48,94 @@ function App() {
   }, []);
 
   const dailyGameReminder = (
-    <div className='text-center'>
-      <p className='font-semibold mb-2'>You already played today!</p>
-      <p className='font-semibold mb-2'>See you again tomorrow :)</p>
+    <div className="text-center">
+      <p className="font-semibold mb-2">You already played today!</p>
+      <p className="font-semibold mb-2">See you again tomorrow :)</p>
       {savedScoreString.current}
     </div>
   );
 
   return (
-    <div className='h-full max-h-svh'>
+    <div className="h-full max-h-svh">
       <ConfigProvider
         theme={{
           algorithm: darkAlgorithm,
         }}
       >
         <Flex
-          style={{ width: '100vw', maxWidth: '1024px' }}
-          align='center'
-          justify='space-between'
-          className='h-full w-full '
+          style={{ width: "100vw", maxWidth: "1024px" }}
+          align="center"
+          justify="space-between"
+          className="h-full w-full "
           vertical
         >
           <Navbar theme={imageTheme} />
+          <Alert
+            message="WE ARE BEING IMPERSONATED ON TWITTER"
+            description="Twitter user @imaginate_ai is using our name and branding to support a crypto scam. Do not invest."
+            type="error"
+            showIcon
+            closable
+          />
+
           <Flex
-            align='center'
-            justify='space-evenly'
-            className='flex-auto w-full'
+            align="center"
+            justify="space-evenly"
+            className="flex-auto w-full"
             vertical
           >
             {showApp ? <PhotoQueue images={images} /> : dailyGameReminder}
           </Flex>
-          <div className='text-center m-8'>
+          <Flex
+            className="text-center m-8"
+            align="center"
+            justify="center"
+          >
             <p>
-              Made by{' '}
+              Follow us on{" "}
               <a
-                href='https://faisal-fawad.github.io'
-                onClick={() => posthog.capture('faisal_link_click')}
-                target='_blank'
+                href="https://bsky.app/profile/playimaginate.bsky.social"
+                onClick={() => posthog.capture("bluesky_link_click")}
+              >
+                bluesky
+              </a>
+            </p>
+            <Divider type="vertical" />
+            <p>
+              Made by{" "}
+              <a
+                href="https://faisal-fawad.github.io"
+                onClick={() => posthog.capture("faisal_link_click")}
+                target="_blank"
               >
                 Faisal
               </a>
-              ,{' '}
+              ,{" "}
               <a
-                href='https://nathanprobert.ca'
-                onClick={() => posthog.capture('nate_link_click')}
-                target='_blank'
+                href="https://nathanprobert.ca"
+                onClick={() => posthog.capture("nate_link_click")}
+                target="_blank"
               >
                 Nate
               </a>
-              , and{' '}
+              , and{" "}
               <a
-                href='https://zachlegesse.ca'
-                onClick={() => posthog.capture('zach_link_click')}
-                target='_blank'
+                href="https://zachlegesse.ca"
+                onClick={() => posthog.capture("zach_link_click")}
+                target="_blank"
               >
                 Zach
               </a>
             </p>
+            <Divider type="vertical" />
             <a
-              className='text-slate-500'
-              href='https://pexels.com'
-              target='_blank'
+              className="text-slate-600 underline"
+              href="https://pexels.com"
+              target="_blank"
             >
-              Real photos sourced at Pexels.com
+              Pexels.com
             </a>
-          </div>
+          </Flex>
         </Flex>
       </ConfigProvider>
     </div>
